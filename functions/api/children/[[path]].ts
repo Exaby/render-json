02dcf86ffddcfa17a813be3fce2498ebbdd27061
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2023-02-09 11:26:09 -0500

    Add files via upload

---
+++ functions/api/children/[[path]].ts
@@ -0,0 +1,27 @@
+import { notFound, parseBucketPath } from "@/utils/bucket";
+
+export async function onRequestGet(context) {
+  try {
+    const [bucket, path] = parseBucketPath(context);
+    const prefix = path && `${path}/`;
+
+    const objList = await bucket.list({
+      prefix,
+      delimiter: "/",
+      include: ["httpMetadata", "customMetadata"],
+    });
+    const objKeys = objList.objects
+      .filter((obj) => !obj.key.endsWith("/_$folder$"))
+      .map((obj) => {
+        const { key, size, uploaded, httpMetadata, customMetadata } = obj;
+        return { key, size, uploaded, httpMetadata, customMetadata };
+      });
+
+    let folders = objList.delimitedPrefixes;
+    return new Response(JSON.stringify({ value: objKeys, folders }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    return new Response(e.toString(), { status: 500 });
+  }
+}
